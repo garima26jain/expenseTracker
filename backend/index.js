2ec00996bd16c239ade57fbcf0c9b6e1b91fbc62
garima26jain <garimajain096@gmail.com>
2024-06-14 20:16:33 +0530

    path updated

--- backend/index.js
+++ backend/index.js
@@ -77,10 +77,10 @@ app.use(
 );
 
 // npm run build will build your frontend app, and it will the optimized version of your app
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/frontend/dist", "index.html"));
 
 });
 
