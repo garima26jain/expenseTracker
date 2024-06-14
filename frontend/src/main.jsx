9f5ef316554996020eefdbbe515b53a7716f8d5a
garima26jain <garimajain096@gmail.com>
2024-06-14 19:17:11 +0530

    deployed url added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,7 +11,7 @@ const client = new ApolloClient({
   uri:
     import.meta.env.VITE_NODE_ENV === "development"
       ? "http://localhost:4000/graphql"
-      : "/graphql", // the URL of our GraphQL server.
+      : "https://expensetracker-fxo7.onrender.com/graphql", // the URL of our GraphQL server.
   cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
   credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
