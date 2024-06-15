398ce71c54e7566c98f7d686a62a7d4f4bbfc83b
garima26jain <garimajain096@gmail.com>
2024-06-15 12:54:04 +0530

    prod url updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ const client = new ApolloClient({
   uri:
     import.meta.env.VITE_NODE_ENV === "development"
       ? "http://localhost:4000/graphql"
-      :"/graphql", // the URL of our GraphQL server.
+      : "expense-tracker-three-alpha.vercel.app/graphql", // the URL of our GraphQL server.
   cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
   credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
