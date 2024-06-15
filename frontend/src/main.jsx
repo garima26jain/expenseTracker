8e63bdef8e5737b1b9e9eaa543272443258ad287
garima26jain <garimajain096@gmail.com>
2024-06-15 12:05:44 +0530

    minor changes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,13 +5,12 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import GridBackground from "./components/ui/GridBackground.jsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
-
+console.log("debug garima", import.meta.env.VITE_NODE_ENV);
 const client = new ApolloClient({
-  // TODO => Update the uri on production
   uri:
     import.meta.env.VITE_NODE_ENV === "development"
       ? "http://localhost:4000/graphql"
-      : "https://expense-tracker-rosy-five.vercel.app/graphql", // the URL of our GraphQL server.
+      : "/graphql", // the URL of our GraphQL server.
   cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
   credentials: "include", // This tells Apollo Client to send cookies along with every request to the server.
 });
